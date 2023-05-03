0c440bd1615e2ef2e4a237a07402421150008b75
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-05-03 21:57:17 +0600

    added login/register ui

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div className='flex justify-center text-center m-20'>
-        <ul className=' border p-20 rounded'>
-            <li className=' text-xl pb-5'>Choose an option to Log In</li>
-            <li><button className=' bg-black text-white rounded px-5 py-2'>Log In with Email/Password</button></li>
-            <li><button className=' bg-black text-white rounded px-5 py-2 my-5'>Log In with Gmail</button></li>
-            <li><button className=' bg-black text-white rounded px-5 py-2 mb-5'>Log In with GitHub</button></li>
-            <li>Do not have an account?<span className=' underline'><Link to="/registration"> Register.</Link></span></li>
-        </ul>
+    <div className='w-4/12 mx-auto p-20 border my-20'>
+      <form action="" className='grid grid-cols-1'>
+        <input type="email" name="email" placeholder='Email ' className='border border-black p-2 ' />
+        <input type="password" name="password" placeholder='Password' className='border border-black p-2 my-2' />
+        <input type="submit" value="Log In" className='border border-black p-2 font-semibold' />
+      </form>
+      <p className='text-center py-10'> <hr /></p>
+      <div className='grid grid-cols-1'>
+        <button className=' bg-black text-white rounded px-5 py-2 mb-2'>Log In with Google</button>
+        <button className=' bg-black text-white rounded px-5 py-2'>Log In with GitHub</button>
+      </div>
+      <p className=' text-center pt-5'>Do not have an account?<span className=' underline'><Link to="/registration">Register</Link></span></p>
     </div>
   )
 }
--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 function Registration() {
+  const [error, setError] = useState('');
+
+  const handleRegistration = event => {
+    event.preventDefault();
+
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const name = form.name.value;
+    const photo = form.text.value;
+    console.log(email, password, name, photo);
+
+    let errors = '';
+
+    if (password.length < 6) {
+      errors += 'Password must have at least 6 characters.';
+    }
+
+    if (!email) {
+      errors += 'Email field cannot be empty.';
+    }
+
+    if (!password) {
+      errors += 'Password field cannot be empty.';
+    }
+
+    setError(errors.trim());
+  }
   return (
     <div>
-        
+      <div className='w-4/12 mx-auto p-20 border my-20'>
+        <form onSubmit={handleRegistration} className='grid grid-cols-1'>
+          <input type="text" name="name" placeholder='Enter your name' className='border border-black p-2 mb-2' />
+          <input type="email" name="email" placeholder='Email ' className='border border-black p-2 ' />
+          <input type="password" name="password" placeholder='Password' className='border border-black p-2 my-2' />
+          <input type="text" name="text" placeholder='Enter your photo url' className='border border-black p-2 ' />
+          <input type="submit" value="Register" className='border border-black p-2 font-semibold mt-2' />
+        </form>
+        <p className=' text-center pt-5'>Already have an account?<span className=' underline'><Link to="/login">Log In</Link></span></p>
+        <p className=' text-center pt-2 text-red-700'>{error}</p>
+      </div>
     </div>
   )
 }
